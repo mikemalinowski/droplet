37a65cf33b7a180bac491b94637d651fb1459863
mikemalinowski <mike.malinowski@outlook.com>
2018-12-17 22:33:31 +0000

    Updating with clion setup

---
+++ src/Auxillary/AuxMath.h
@@ -0,0 +1,134 @@
+/*
+
+This holds a bunch of math utility functions which are useful
+across multiple nodes/commands.
+
+Author : Mike Malinowski : www.twisted.space
+*/
+#pragma once
+#include <maya/MPxNode.h>
+#include <maya/MArrayDataHandle.h>
+#include <maya/MGlobal.h>
+#include <maya/MMatrix.h>
+#include <maya/MVector.h>
+#include <maya/MQuaternion.h>
+#include <maya/MFloatVector.h>
+#include <maya/MEulerRotation.h>
+#include <maya/MTransformationMatrix.h>
+#include <maya/MFnNumericAttribute.h>
+#include <maya/MFnUnitAttribute.h>
+#include <maya/MFnCompoundAttribute.h>
+#include <maya/MFnMatrixAttribute.h>
+
+
+namespace AuxMath
+{
+	/* ---------------------------------------------------------------------- */
+	MVector RotationFromTransform(MTransformationMatrix transform, double x_offset=0, double y_offset=0, double z_offset=0)
+	{
+		MEulerRotation rotation = transform.eulerRotation();
+
+		// -- Roll is in degree's, so convert to degrees to add the roll in and then
+		// -- convert it back to radians
+		rotation.x = ((rotation.x * 57.2957795) + x_offset) * 0.0174532925;
+		rotation.y = ((rotation.y * 57.2957795) + y_offset) * 0.0174532925;
+		rotation.z = ((rotation.z * 57.2957795) + z_offset) * 0.0174532925;
+
+		MVector vector(
+			rotation.x,
+			rotation.y,
+			rotation.z
+		);
+
+		return vector;
+	};
+
+	/* ---------------------------------------------------------------------- */
+	MMatrix RelativeMatrix(MMatrix matrix, MMatrix parentWorldSpaceMatrix)
+	{
+		MMatrix relativeMat4 = matrix * parentWorldSpaceMatrix.inverse();
+		return relativeMat4;
+	};
+	
+	/* ---------------------------------------------------------------------- */
+	MTransformationMatrix FormTransform(MMatrix rotationMatrix, MVector translationVector)
+	{
+		MTransformationMatrix transform = MTransformationMatrix(rotationMatrix);
+		transform.setTranslation(translationVector, MSpace::kWorld);
+		return transform;
+	}
+
+	/* ---------------------------------------------------------------------- */
+	/*
+	Takes in two Transformation Matrices and extracts out the positions.
+	We then return a relative position back.
+	*/
+	MVector LerpTransformPositions(MTransformationMatrix transformA, MTransformationMatrix transformB, double blend)
+	{
+		// -- Get the relative vector between the two
+		// -- transforms
+		MVector positionA = transformA.getTranslation(MSpace::kWorld);
+		MVector positionB = transformB.getTranslation(MSpace::kWorld);
+		MVector delta = positionA - positionB;
+
+		// -- Factor in the blend and add in the 
+		// -- first posisiton to make it relative
+		delta = (delta * blend) + positionA;
+		
+		return delta;
+	}
+
+	/* ---------------------------------------------------------------------- */
+	MVector CrossVectors(MVector vectorA, MVector vectorB)
+	{
+		MVector outVector = vectorA ^ vectorB;
+
+		outVector.normalize();
+
+		return outVector;
+	}
+
+	/* ---------------------------------------------------------------------- */
+	/*
+	Quaternion Slerp function. Implementation taken from:
+	http://www.euclideanspace.com/maths/algebra/realNormedAlgebra/quaternions/slerp/index.htm
+	*/
+	MQuaternion SlerpQuaternion(MQuaternion qa, MQuaternion qb, double t)
+	{
+		// quaternion to return
+		MQuaternion qm = MQuaternion();
+
+		// Calculate angle between them.
+		double cosHalfTheta = qa.w * qb.w + qa.x * qb.x + qa.y * qb.y + qa.z * qb.z;
+
+		// if qa=qb or qa=-qb then theta = 0 and we can return qa
+		if (abs(cosHalfTheta) >= 1.0) {
+			qm.w = qa.w; qm.x = qa.x; qm.y = qa.y; qm.z = qa.z;
+			return qm;
+		}
+
+		// Calculate temporary values.
+		double halfTheta = acos(cosHalfTheta);
+		double sinHalfTheta = sqrt(1.0 - cosHalfTheta*cosHalfTheta);
+
+		// if theta = 180 degrees then result is not fully defined
+		// we could rotate around any axis normal to qa or qb
+		if (fabs(sinHalfTheta) < 0.001) { // fabs is floating point absolute
+			qm.w = (qa.w * 0.5 + qb.w * 0.5);
+			qm.x = (qa.x * 0.5 + qb.x * 0.5);
+			qm.y = (qa.y * 0.5 + qb.y * 0.5);
+			qm.z = (qa.z * 0.5 + qb.z * 0.5);
+			return qm;
+		}
+
+		double ratioA = sin((1 - t) * halfTheta) / sinHalfTheta;
+		double ratioB = sin(t * halfTheta) / sinHalfTheta;
+
+		//calculate Quaternion.
+		qm.w = (qa.w * ratioA + qb.w * ratioB);
+		qm.x = (qa.x * ratioA + qb.x * ratioB);
+		qm.y = (qa.y * ratioA + qb.y * ratioB);
+		qm.z = (qa.z * ratioA + qb.z * ratioB);
+		return qm;
+	}
+}
---
+++ src/Nodes/Rigging/QuaternionTwist.cpp
@@ -0,0 +1,225 @@
+/*
+
+This is a twist node that utilises quaternions to create the 
+twist behaviour. Its a bit of a mish-mash of code utilising 
+various techniques demonstrated over a variety of rigging 
+and math websites.
+
+Author : Mike Malinowski : www.twisted.space
+*/
+
+// Maya Includes
+#include <maya/MPxNode.h>
+#include <maya/MGlobal.h>
+#include <maya/MFloatVector.h>
+#include <maya/MTransformationMatrix.h>
+#include <maya/MMatrix.h>
+#include <maya/MQuaternion.h>
+#include <maya/MVector.h>
+#include <maya/MEulerRotation.h>
+#include <maya/MFnNumericAttribute.h>
+#include <maya/MFnUnitAttribute.h>
+#include <maya/MFnCompoundAttribute.h>
+#include <maya/MFnMatrixAttribute.h>
+
+// Droplet Includes
+#include "../../Auxillary/AuxMath.h"
+#include "QuaternionTwist.h"
+
+
+// Defining our input attributes
+MObject QuaternionTwist::Root;
+MObject QuaternionTwist::Target;
+MObject QuaternionTwist::Twist;
+MObject QuaternionTwist::Offset;
+MObject QuaternionTwist::ParentMatrix;
+
+// Defining our output attributes
+MObject QuaternionTwist::rotation;
+MObject QuaternionTwist::translation;
+
+
+/* ---------------------------------------------------------------------- */
+void* QuaternionTwist::creator()
+{
+	return new QuaternionTwist;
+}
+
+
+/* ---------------------------------------------------------------------- */
+MStatus QuaternionTwist::initialize()
+{
+	// Declare the attribute types we will use
+	MFnMatrixAttribute   matrixAttrFn;
+	MFnNumericAttribute  numericAttrFn;
+	MFnUnitAttribute     unitAttrFn;
+	MFnCompoundAttribute compoundAttrFn;
+
+	// Add the input attributes
+	QuaternionTwist::Root = matrixAttrFn.create("root", "roo");
+	CHECK_MSTATUS_AND_RETURN(addAttribute(QuaternionTwist::Root), MStatus::kFailure);
+
+	QuaternionTwist::Target = matrixAttrFn.create("target", "trg");
+	CHECK_MSTATUS_AND_RETURN(addAttribute(QuaternionTwist::Target), MStatus::kFailure);
+
+	QuaternionTwist::Twist = numericAttrFn.create("twist", "tws", MFnNumericData::kDouble);
+	numericAttrFn.setKeyable(true);
+	CHECK_MSTATUS_AND_RETURN(addAttribute(QuaternionTwist::Twist), MStatus::kFailure);
+
+	QuaternionTwist::Offset = numericAttrFn.create("offset", "ofs", MFnNumericData::kDouble);
+	numericAttrFn.setKeyable(true);
+	CHECK_MSTATUS_AND_RETURN(addAttribute(QuaternionTwist::Offset), MStatus::kFailure);
+
+	QuaternionTwist::ParentMatrix = matrixAttrFn.create("parentWorldSpace", "prnt");
+	CHECK_MSTATUS_AND_RETURN(addAttribute(QuaternionTwist::ParentMatrix), MStatus::kFailure);
+
+	// Create the output attributes
+	MObject rX = unitAttrFn.create("rotationX", "rx", MFnUnitAttribute::kAngle);
+	MObject rY = unitAttrFn.create("rotationY", "ry", MFnUnitAttribute::kAngle);
+	MObject rZ = unitAttrFn.create("rotationZ", "rz", MFnUnitAttribute::kAngle);
+
+	QuaternionTwist::rotation = compoundAttrFn.create("rotation", "ro");
+	compoundAttrFn.addChild(rX);
+	compoundAttrFn.addChild(rY);
+	compoundAttrFn.addChild(rZ);
+	CHECK_MSTATUS_AND_RETURN(addAttribute(QuaternionTwist::rotation), MStatus::kFailure);
+
+	QuaternionTwist::translation = numericAttrFn.createPoint("translation", "ot");
+	CHECK_MSTATUS_AND_RETURN(addAttribute(QuaternionTwist::translation), MStatus::kFailure);
+
+	// Setup the attribute dependencies
+	attributeAffects(QuaternionTwist::Root, QuaternionTwist::rotation);
+	attributeAffects(QuaternionTwist::Target, QuaternionTwist::rotation);
+	attributeAffects(QuaternionTwist::Twist, QuaternionTwist::rotation);
+	attributeAffects(QuaternionTwist::ParentMatrix, QuaternionTwist::rotation);
+	attributeAffects(QuaternionTwist::Offset, QuaternionTwist::rotation);
+
+	attributeAffects(QuaternionTwist::Root, QuaternionTwist::translation);
+	attributeAffects(QuaternionTwist::Target, QuaternionTwist::translation);
+	attributeAffects(QuaternionTwist::Twist, QuaternionTwist::translation);
+	attributeAffects(QuaternionTwist::ParentMatrix, QuaternionTwist::translation);
+	attributeAffects(QuaternionTwist::Offset, QuaternionTwist::translation);
+
+
+	// Declare the attribute types we will 
+	// be using
+	return MS::kSuccess;
+}
+
+
+/* ---------------------------------------------------------------------- */
+MStatus QuaternionTwist::compute(const MPlug& plug, MDataBlock& dataBlock)
+{
+
+	// Read out all the data we need to calculate
+	// the twist
+	MMatrix rootMatrix = dataBlock.inputValue(QuaternionTwist::Root).asMatrix();
+	MMatrix targetMatrix = dataBlock.inputValue(QuaternionTwist::Target).asMatrix();
+	MMatrix parentWorldMatrix = dataBlock.inputValue(QuaternionTwist::ParentMatrix).asMatrix();
+	double twist = dataBlock.inputValue(QuaternionTwist::Twist).asDouble();
+	double offset = dataBlock.inputValue(QuaternionTwist::Offset).asDouble();
+
+	// Utilise transformation matrices during the compute
+	MTransformationMatrix transformA(rootMatrix);
+	MTransformationMatrix transformB(targetMatrix);
+
+	// -- Split the rotation and the position and lerp/slerp them
+	MMatrix rotation_matrix = QuaternionTwist::RotationMatrix(transformA, transformB, twist);
+	MVector position_vector = AuxMath::LerpTransformPositions(transformA, transformB, twist);
+
+	// -- Rebuild the 4x4 matrix
+	MMatrix twistedWorldSpaceMatrix = AuxMath::FormTransform(rotation_matrix, position_vector).asMatrix();
+
+	// Get the relative matrix between the twistMat4 and the parent global
+	MMatrix twistedMatrix = AuxMath::RelativeMatrix(twistedWorldSpaceMatrix, parentWorldMatrix);
+
+	// -- Extract the data we need
+	MTransformationMatrix twistedTransform(twistedMatrix);
+	MVector rotationVector = AuxMath::RotationFromTransform(twistedTransform, offset);
+	MFloatVector translationVector(twistedTransform.getTranslation(MSpace::kWorld));
+
+	// -- Set the output plugs
+	dataBlock.outputValue(QuaternionTwist::rotation).set(rotationVector);
+	dataBlock.outputValue(QuaternionTwist::translation).set(translationVector);
+
+	// -- Mark the two attributes as being clean (solved)
+	dataBlock.setClean(QuaternionTwist::rotation);
+	dataBlock.setClean(QuaternionTwist::translation);
+	return MStatus::kSuccess;
+}
+
+
+/* ---------------------------------------------------------------------- */
+MMatrix QuaternionTwist::RotationMatrix(MTransformationMatrix transformA, MTransformationMatrix transformB, double twistFactor)
+{
+	MQuaternion slerpedQuaternion = QuaternionTwist::SlerpInterpolation(transformA, transformB, twistFactor);
+	MVector     altAxis = QuaternionTwist::ExtractAxis(slerpedQuaternion, 2);
+
+	MVector     axisX = QuaternionTwist::SubtractionVector(transformA, transformB);
+	MVector     axisZ = AuxMath::CrossVectors(altAxis, axisX);
+	MVector     axisY = AuxMath::CrossVectors(axisZ, axisX);
+
+	MMatrix rotationMatrix = MMatrix();
+
+	rotationMatrix[0][0] = axisX.x;
+	rotationMatrix[0][1] = axisX.y;
+	rotationMatrix[0][2] = axisX.z;
+
+	rotationMatrix[1][0] = axisY.x;
+	rotationMatrix[1][1] = axisY.y;
+	rotationMatrix[1][2] = axisY.z;
+
+	rotationMatrix[2][0] = axisZ.x;
+	rotationMatrix[2][1] = axisZ.y;
+	rotationMatrix[2][2] = axisZ.z;
+
+	return rotationMatrix;
+}
+
+
+/* ---------------------------------------------------------------------- */
+MQuaternion QuaternionTwist::SlerpInterpolation(MTransformationMatrix transformA, MTransformationMatrix transformB, double twistFactor)
+{
+	double xA, yA, zA, wA;
+	double xB, yB, zB, wB;
+
+	transformA.getRotationQuaternion(xA, yA, zA, wA);
+	transformB.getRotationQuaternion(xB, yB, zB, wB);
+
+	MQuaternion quatA(xA, yA, zA, wA);
+	MQuaternion quatB(xB, yB, zB, wB);
+
+	return AuxMath::SlerpQuaternion(quatA, quatB, twistFactor);
+}
+
+
+/* ---------------------------------------------------------------------- */
+MVector QuaternionTwist::SubtractionVector(MTransformationMatrix transformA, MTransformationMatrix transformB)
+{
+	MVector vecA = transformA.getTranslation(MSpace::kWorld);
+	MVector vecB = transformB.getTranslation(MSpace::kWorld);
+
+	MVector sub = vecA - vecB;
+
+	sub.normalize();
+
+	return sub;
+}
+
+
+/* ---------------------------------------------------------------------- */
+MVector QuaternionTwist::ExtractAxis(MQuaternion q, int row_id)
+{
+	MTransformationMatrix transform = MTransformationMatrix();
+	transform.setRotationQuaternion(q.x, q.y, q.z, q.w);
+	MMatrix rotationMat4 = transform.asRotateMatrix();
+
+	MVector outVector = MVector();
+
+	outVector.x = rotationMat4[row_id][0];
+	outVector.y = rotationMat4[row_id][1];
+	outVector.z = rotationMat4[row_id][2];
+
+	return outVector;
+}
+
